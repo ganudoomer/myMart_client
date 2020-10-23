d63eff91e8ade51f6d35dd6bd73073de5e241209
ganudoomer <ganuiscool@gmail.com>
2020-10-23 15:06:55 +0530

    Fixed App Drawer

--- src/container/user/layout/drawer.js
+++ src/container/user/layout/drawer.js
@@ -8,14 +8,12 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from 'react-router-dom';
 import Shop from '../../../Buy.svg';
 import IconButton from '@material-ui/core/IconButton';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
-import Typography from '@material-ui/core/Typography';
 import DescriptionIcon from '@material-ui/icons/Description';
 
 const useStyles = makeStyles({
@@ -40,48 +38,8 @@ export default function TemporaryDrawer(props) {
 
 		setState({ ...state, [anchor]: open });
 	};
-	let button = (
-		<React.Fragment>
-			<Link style={{ textDecoration: 'none' }} to="/login">
-				<Button className={classes.button}>Login</Button>
-			</Link>
-			<Divider />
-			<Link style={{ textDecoration: 'none' }} to="/register">
-				<Button className={classes.button}>Register</Button>
-			</Link>
-		</React.Fragment>
-	);
-	if (props.token) {
-		button = (
-			<React.Fragment>
-				<Link to="/cart" style={{ textDecoration: 'none' }}>
-					<IconButton>
-						<Typography variant="subtitle1">{props.count}</Typography>
-						<img alt="another Logo" width="30px" src={Shop} />
-					</IconButton>
-				</Link>
-				<Divider />
-				<Link to="/history" style={{ textDecoration: 'none' }}>
-					<IconButton>
-						<DescriptionIcon style={{ fontSize: 30 }} />
-					</IconButton>
-				</Link>
-				<Divider />
-				<Link to="/live" style={{ textDecoration: 'none' }}>
-					<IconButton>
-						<ShoppingBasketIcon style={{ fontSize: 25 }} />
-						Live
-					</IconButton>
-				</Link>
-				<Divider />
-				<Link style={{ textDecoration: 'none' }} onClick={() => props.onLogout()} to="/logout">
-					<Button className={classes.button}>Logout</Button>
-				</Link>
-			</React.Fragment>
-		);
-	}
 
-	const list = (anchor) => (
+	let list = (anchor) => (
 		<div
 			className={clsx(classes.list, {
 				[classes.fullList]: anchor === 'top' || anchor === 'bottom'
@@ -90,9 +48,86 @@ export default function TemporaryDrawer(props) {
 			onClick={toggleDrawer(anchor, false)}
 			onKeyDown={toggleDrawer(anchor, false)}
 		>
-			<List>{button}</List>
+			<List>
+				<Link style={{ textDecoration: 'none' }} to="/login">
+					<ListItem button>
+						<ListItemIcon>
+							<ListItemText primary="Login" />
+						</ListItemIcon>
+					</ListItem>
+				</Link>
+				<Divider />
+				<Link style={{ textDecoration: 'none' }} to="/register">
+					<ListItem button>
+						<ListItemIcon>
+							<ListItemText primary="Register" />
+						</ListItemIcon>
+					</ListItem>
+				</Link>
+				<Divider />
+			</List>
 		</div>
 	);
+
+	if (props.token) {
+		list = (anchor) => (
+			<div
+				className={clsx(classes.list, {
+					[classes.fullList]: anchor === 'top' || anchor === 'bottom'
+				})}
+				role="presentation"
+				onClick={toggleDrawer(anchor, false)}
+				onKeyDown={toggleDrawer(anchor, false)}
+			>
+				<List>
+					<Link to="/cart" style={{ textDecoration: 'none' }}>
+						<ListItem button>
+							<IconButton>
+								<img alt="another Logo" width="30px" src={Shop} />
+							</IconButton>
+							<ListItemIcon>
+								<ListItemText primary="Cart" />
+							</ListItemIcon>
+						</ListItem>
+					</Link>
+					<Divider />
+					<Link to="/history" style={{ textDecoration: 'none' }}>
+						<ListItem button>
+							<IconButton>
+								<DescriptionIcon style={{ fontSize: 30 }} />
+							</IconButton>
+							<ListItemIcon>
+								<ListItemText primary="Order History" />
+							</ListItemIcon>
+						</ListItem>
+					</Link>
+					<Divider />
+
+					<Link to="/live" style={{ textDecoration: 'none' }}>
+						<ListItem button>
+							<IconButton>
+								<ShoppingBasketIcon style={{ fontSize: 25 }} />
+							</IconButton>
+							<ListItemIcon>
+								<ListItemText primary="Live Orders" />
+							</ListItemIcon>
+						</ListItem>
+					</Link>
+					<Divider />
+					<Link style={{ textDecoration: 'none' }} onClick={() => props.onLogout()} to="/logout">
+						<ListItem button>
+							<IconButton>
+								<ExitToAppIcon style={{ fontSize: 25 }} />
+							</IconButton>
+							<ListItemIcon>
+								<ListItemText primary="Live Orders" />
+							</ListItemIcon>
+						</ListItem>
+					</Link>
+				</List>
+			</div>
+		);
+	}
 
 	return (
 		<div>
